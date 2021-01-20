34c17fa3ee024eda43d0b08c429b169f30a73ff6
bguamed356 <bguamed356@iessaladillo.es>
2021-01-20 09:17:51 +0100

    No se lo que hago

---
+++ PruebaGit1_BG/src/Cambio/Cambio1.java
@@ -0,0 +1,10 @@
+package Cambio;
+
+public class Cambio1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Cadena nueva");
+	}
+
+}
--- PruebaGit1_BG/src/Principal1/Principal1.java
+++ PruebaGit1_BG/src/Principal1/Principal1.java
@@ -4,6 +4,7 @@ public class Principal1 {
 
 	public static void main(String[] args) {
 
+		System.out.println("Git en Java");
 	}
 
 }
